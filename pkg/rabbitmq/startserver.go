8c98bc62e56763ce930353afed35a3bd91f63cc5
Steven Jansen <stevenjansen180@gmail.com>
2022-12-12 23:29:38 +0100

    a

--- pkg/rabbitmq/startserver.go
+++ pkg/rabbitmq/startserver.go
@@ -1,54 +1,48 @@
 package rabbitmq
 
-import (
-	"log"
+// func StartServer() {
+// 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+// 	failOnError(err, "Failed to connect to RabbitMQ")
+// 	defer conn.Close()
 
-	amqp "github.com/rabbitmq/amqp091-go"
-)
+// 	ch, err := conn.Channel()
+// 	failOnError(err, "Failed to open a channel")
+// 	defer ch.Close()
 
-func StartServer() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+// 	q, err := ch.QueueDeclare(
+// 		"hello", // name
+// 		false,   // durable
+// 		false,   // delete when unused
+// 		false,   // exclusive
+// 		false,   // no-wait
+// 		nil,     // arguments
+// 	)
+// 	failOnError(err, "Failed to declare a queue")
+// 	msgs, err := ch.Consume(
+// 		q.Name, // queue
+// 		"",     // consumer
+// 		true,   // auto-ack
+// 		false,  // exclusive
+// 		false,  // no-local
+// 		false,  // no-wait
+// 		nil,    // args
+// 	)
+// 	failOnError(err, "Failed to register a consumer")
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+// 	var forever chan struct{}
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
+// 	go func() {
+// 		for d := range msgs {
+// 			log.Printf("Received a message: %s", d.Body)
+// 		}
+// 	}()
 
-	var forever chan struct{}
+// 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+// 	<-forever
+// }
 
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
-}
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
+// func failOnError(err error, msg string) {
+// 	if err != nil {
+// 		log.Panicf("%s: %s", msg, err)
+// 	}
+// }
