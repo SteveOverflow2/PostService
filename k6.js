d2c05dbe1eb98ef0365fba07eeb4da16577ebac7
Steven Jansen <stevenjansen180@hotmail.com>
2022-12-18 11:29:09 +0100

    a

--- k6.js
+++ k6.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import exec from 'k6/execution';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
   stages: [
@@ -11,7 +12,7 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('http://34.77.52.21/posts/api/post/');
+  const res = http.get('http://34.77.52.21/posts/api/post/getAll');
   if(!check(res, { 'status must be 200': (r) => r.status == 200 })){
     exec.test.abort()
   }
@@ -21,7 +22,7 @@ export default function () {
 
 export function handleSummary(data) {
   console.log('Finished executing performance tests');
-
+  
   return {
     'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
   };
