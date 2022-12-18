50243a288a26c86ded4daacc33a650ef9e7a50e1
Steven Jansen <stevenjansen180@hotmail.com>
2022-12-18 11:26:51 +0100

    ae

--- k6.js
+++ k6.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import exec from 'k6/execution';
 
 export const options = {
   stages: [
@@ -12,7 +13,7 @@ export const options = {
 export default function () {
   const res = http.get('http://34.77.52.21/posts/api/post/');
   if(!check(res, { 'status must be 200': (r) => r.status == 200 })){
-    test.abort()
+    exec.test.abort()
   }
   sleep(1);
 }
