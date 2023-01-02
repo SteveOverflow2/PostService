769727b302f6337af2fb2060ccd4976972db416b
Steven Jansen <stevenjansen180@gmail.com>
2023-01-02 17:05:23 +0100

    a

--- pkg/http/rest/middleware.go
+++ pkg/http/rest/middleware.go
@@ -5,6 +5,7 @@ import "net/http"
 func (s *server) Cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		(w).Header().Add("Access-Control-Allow-Origin", "*")
+
 		(w).Header().Set("Content-Type", "text/html; charset=utf-8")
 
 		h.ServeHTTP(w, r.WithContext(r.Context()))
