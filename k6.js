37c8c617bc459139bd3e51454ffa805f8333a0ce
Steven Jansen <stevenjansen180@gmail.com>
2023-01-02 14:54:13 +0100

    a

--- k6.js
+++ k6.js
@@ -5,28 +5,26 @@ import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
   stages: [
+    { duration: '5s', target: 2000 },
     { duration: '10s', target: 1000 },
-    { duration: '10s', target: 4000 },
-  { duration: '5s', target: 1000 },
-  { duration: '1s', target: 10 },
+    { duration: '4s', target: 1 },
   ],
 };
 
 export default function () {
   const res = http.get('http://34.77.52.21/posts/api/post/getAll');
-  if (!check(res, { 'status must be 200': (r) => r.status == 200 })) {
-    console.log("Got back statuscode: " + res.status)
-  }
+  // if(!check(res, { 'status must be 200': (r) => r.status == 200 })){
+  //   exec.test.abort()
+  // }
   sleep(1);
-  
 }
 
 
 export function handleSummary(data) {
   console.log('Finished executing performance tests');
-
+  
   return {
     'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
-    'summary.txt': textSummary(data, { indent: ' ' }), // and a JSON with all the details...
+    'summary.txt': textSummary(data, { indent: ' '}), // and a JSON with all the details...
   };
 }
