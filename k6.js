15bf9a36a964380691cc97dcdec6568880040cd8
Steven Jansen <stevenjansen180@gmail.com>
2023-01-08 16:10:07 +0100

    a

--- k6.js
+++ k6.js
@@ -5,20 +5,16 @@ import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
   stages: [
-    { duration: '10s', target: 200 },
-    { duration: '10s', target: 1000 },
-    { duration: '30s', target: 2000 },
-    { duration: '10s', target: 1000 },
-    { duration: '4s', target: 0 },
+    { duration: '500s', target: 5000 },
   ],
 };
 
 export default function () {
   const res = http.get('http://34.77.52.21/posts/api/post/getAll');
-  // if(!check(res, { 'status must be 200': (r) => r.status == 200 })){
+  // if(!check(res, { 'status must be 20000': (r) => r.status == 200 })){
   //   exec.test.abort()
   // }
-  sleep(1);
+  sleep(5);
 }
 
 
